00685dbcff872d3a2e721f000d24e8e7305c1d53
SamDunnachie <samd@3logix.com.au>
2025-04-23 09:09:24 +1200

    Update version

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import logo from './img/3lgx-swirl-logo.png';
 function Header() {
   return (
     <header>
-      <span id="version">v0.2.5.3</span>
+      <span id="version">v0.2.5.4</span>
       <h1>Cannon <span id="white">Hill</span></h1>
       <img src={logo}></img>
     </header>
