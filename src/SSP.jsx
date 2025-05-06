bb177487bf8f92dcad755a16cece3d95b9c39dc0
SamDunnachie <samd@3logix.com.au>
2025-05-06 15:59:16 +1200

    Remove broken services getter

--- src/SSP.jsx
+++ src/SSP.jsx
@@ -2,52 +2,6 @@ import { useEffect, useState } from 'react';
 import './style/SSP.css';
 
 function SSP() {
-
-  
-  const [services, setServices] = useState();
-  const [servicesChildren, setServicesChildren] = useState([]);
-  let tempServices = [];
-
-  function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-
-        const observer = new MutationObserver(() => {
-            if (document.querySelector(selector)) {
-                observer.disconnect();
-                return resolve(document.querySelector(selector));
-            }
-        });
-
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-  }
-
-  waitForElm('.wtss-service-menu').then((elm) => {
-    console.log('Services menu found.');
-    setServices(elm);
-    setServicesChildren(services.children);
-
-    
-    for(let i = 0; i < servicesChildren.length; i++) {
-      tempServices.push(servicesChildren[i]);
-      if (i > 4) {
-        tempServices[i].hidden=true;
-      }
-    }
-    return tempServices;
-  })
-  .then(ts => {
-    console.log(ts);
-    // setServicesChildren(ts);
-    // console.log(servicesChildren);
-    
-  });
   
   return (
     <>
